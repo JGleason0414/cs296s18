d97342a61e065602ae46330759f21323fda86a4c
Thyago Mota <motat@moravian.edu>
2018-01-31 12:51:34 -0500

    grid progr.

---
+++ grid_programmatically/app/src/main/java/edu/moravian/mathcs/grid_programmatically/MainActivity.java
@@ -0,0 +1,41 @@
+package edu.moravian.mathcs.grid_programmatically;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    static final int COLS = 5;
+    static final int ROWS = 2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // grid layout
+        GridLayout gridLayout = new GridLayout(this);
+        gridLayout.setColumnCount(COLS);
+        gridLayout.setRowCount(ROWS);
+
+        // buttons
+        for (int i = 0; i < ROWS; i++)
+            for (int j = 0; j < COLS; j++) {
+                final Button btn = new Button(this);
+                btn.setText(i + "," + j);
+                btn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast toast = Toast.makeText(getBaseContext(), btn.getText().toString() + " was clicked!", Toast.LENGTH_LONG);
+                        toast.show();
+                    }
+                });
+                gridLayout.addView(btn);
+            }
+
+        setContentView(gridLayout);
+    }
+}
