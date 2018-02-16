cd24d5021b515e5fcb566f22102ceac949bd1ed4
Thyago Mota <motat@moravian.edu>
2018-02-15 20:20:41 -0500

    list_view

---
+++ list_view/app/src/main/java/edu/moravian/mathcs/list_view/MainActivity.java
@@ -0,0 +1,29 @@
+package edu.moravian.mathcs.list_view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private List<Student> listStudents;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listStudents = new ArrayList<>();
+        listStudents.add(new Student("johndoe@xyz.com", "John Doe"));
+        listStudents.add(new Student("maryjane@xyz.com", "Mary Jane"));
+        listStudents.add(new Student("sammaitai@xyz.com", "Sam Mai Tai"));
+
+        ListView lvStudents = (ListView) findViewById(R.id.lvStudents);
+        ArrayAdapter<Student> adapter = new ArrayAdapter<Student>(this, android.R.layout.simple_list_item_1, listStudents);
+        lvStudents.setAdapter(adapter);
+    }
+}
